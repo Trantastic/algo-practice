bfe7405e3594e506a6151e8a4bd8a973267540ac
Yvette <yvettetran@gmail.com>
2018-08-30 13:25:37 -0700

    added 2nd possible solution

--- simpleSumArr.js
+++ simpleSumArr.js
@@ -1,15 +1,23 @@
 // Create a function that sums all the elements of an array
 
 function sumArr(arr) {
-	var totalSum = 0;
+	var sum = 0;
 
-	for(i of arr) {
-		totalSum = totalSum + i;
-
-		if(i == arr.length) {
-			console.log(totalSum);
-		}
+	for(let i of arr) {
+		sum = sum + i;
 	}
+	console.log(sum);
 }
 
-sumArr([1,2,3]);
+sumArr([1,2,100]);
+
+function simpleArraySum(ar) {
+    var totalSum = ar.reduce(sum);
+    
+    function sum(total, num) {
+        return total + num;
+    }
+    console.log(totalSum);
+}
+
+simpleArraySum([1,2]);
