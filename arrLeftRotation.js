d320658508104c03558927db6de76a0aaa0d0c4f
Yvette <yvettetran@gmail.com>
2018-08-31 13:38:10 -0700

    created psuedo-code

---
+++ arrLeftRotation.js
@@ -0,0 +1,10 @@
+// Given the integers in an arr, perform x rotatations starting left to right
+// I.e. var arr = [1,2,3,4,5] (rotate 2) => [3,4,5,1,2]
+
+function arrRotation(a, b) {
+/*
+
+	
+
+*/
+}
