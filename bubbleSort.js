866332609ab1dc72f36818e2e19dafaefee867f3
Yvette <yvettetran@gmail.com>
2018-09-04 18:51:19 -0700

    created outline

---
+++ bubbleSort.js
@@ -0,0 +1,10 @@
+// Recreate a version of bubble sort
+/* Once you create the sorting function print the following 3 lines:
+	-print how many swaps took place
+	-first element in the sorted array
+	-last element in the sorted array
+*/
+
+function bubbleSort(arr) {
+	
+}
