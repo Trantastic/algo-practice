e50b68688ad0740668d4c2ca5d9b44c8053def2a
Yvette <yvettetran@gmail.com>
2018-09-07 21:38:06 -0700

    still figuring out bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -9,12 +9,18 @@ function bubbleSort(arr) {
 	var sortedArr = arr;
 	var swapNum = 0;
 
-	for(var i = 0; i < arr.length; i++) {
-		for(var x = 0; x < (arr.length - 1); x++) {
-			if(arr[x] > arr[x + 1]) {
-				arr[x + 1] = arr[x];
+	for(var i = 0; i < sortedArr.length; i++) {
+		for(var x = 0; x < (sortedArr.length - 1); x++) {
+			if(sortedArr[x] > sortedArr[x + 1]) {
+				sortedArr[x + 1] = sortedArr[x];
 				swapNum++;
 			}
 		}
+
+		if(i == (sortedArr.length - 1)) {
+			console.log("Swapped: " + swapNum + " 1st Array: " + sortedArr[0]);
+		}
 	}
 }
+
+bubbleSort([5,4,3,1,2,9]);
